889e58062cd851d44114edc90aa5d86784ee9a4f
Rick <53375860+modularsamples@users.noreply.github.com>
2020-06-27 23:31:04 +0100

    Demos

---
+++ WebsiteDemo/FX/file10.js
@@ -0,0 +1,47 @@
+var keys = new Tone.Sampler({
+		
+			"C1" : "file1.[mp3|ogg]",
+	        "C#1" : "file2.[mp3|ogg]",
+	        "D1" : "file3.[mp3|ogg]",
+			"D#1" : "file4.[mp3|ogg]",
+			"E1" : "file5.[mp3|ogg]",
+			"F1" : "file6.[mp3|ogg]",
+			"F#1" : "file7.[mp3|ogg]",
+			"G1" : "file8.[mp3|ogg]",
+			"G#1" : "file9.[mp3|ogg]",
+			"A1" : "file10.[mp3|ogg]",
+			"A#1" : "file11.[mp3|ogg]",
+			"B1" : "file12.[mp3|ogg]",
+			"C2" : "file13.[mp3|ogg]",
+	        "C#2" : "file14.[mp3|ogg]",
+	        "D2" : "file15.[mp3|ogg]",
+			"D#2" : "file16.[mp3|ogg]",
+			"E2" : "file17.[mp3|ogg]",
+			"F2" : "file18.[mp3|ogg]",
+			"F#2" : "file19.[mp3|ogg]",
+			"G2" : "file20.[mp3|ogg]",
+			"G#2" : "file21.[mp3|ogg]",
+			"A2" : "file22.[mp3|ogg]",
+			"A#2" : "file23.[mp3|ogg]",
+			"B2" : "file24.[mp3|ogg]",
+			"C3" : "file25.[mp3|ogg]",
+	        "C#3" : "file26.[mp3|ogg]",
+	        "D3" : "file27.[mp3|ogg]",
+			"D#3" : "file28.[mp3|ogg]",
+			"E3" : "file29.[mp3|ogg]",
+			"F3" : "file30.[mp3|ogg]",
+			"F#3" : "file31.[mp3|ogg]",
+			"G3" : "file32.[mp3|ogg]",
+			"G#3" : "file33.[mp3|ogg]",
+			"A3" : "file34.[mp3|ogg]",
+			"A#3" : "file35.[mp3|ogg]",
+			"B3" : "file36.[mp3|ogg]",
+			"C4" : "file37.[mp3|ogg]"
+	
+				
+				
+					}, {
+
+						"baseUrl" : "/presets/jp4/FX/SexySynthesizer/"
+					})					.connect(filter);
+					
---
+++ WebsiteDemo/Misc/file6.js
@@ -0,0 +1,47 @@
+var keys = new Tone.Sampler({
+		
+			"C1" : "file37.[mp3|ogg]",
+	        "C#1" : "file36.[mp3|ogg]",
+	        "D1" : "file35.[mp3|ogg]",
+			"D#1" : "file34.[mp3|ogg]",
+			"E1" : "file33.[mp3|ogg]",
+			"F1" : "file32.[mp3|ogg]",
+			"F#1" : "file31.[mp3|ogg]",
+			"G1" : "file30.[mp3|ogg]",
+			"G#1" : "file29.[mp3|ogg]",
+			"A1" : "file28.[mp3|ogg]",
+			"A#1" : "file27.[mp3|ogg]",
+			"B1" : "file26.[mp3|ogg]",
+			"C2" : "file25.[mp3|ogg]",
+	        "C#2" : "file24.[mp3|ogg]",
+	        "D2" : "file23.[mp3|ogg]",
+			"D#2" : "file22.[mp3|ogg]",
+			"E2" : "file21.[mp3|ogg]",
+			"F2" : "file20.[mp3|ogg]",
+			"F#2" : "file19.[mp3|ogg]",
+			"G2" : "file18.[mp3|ogg]",
+			"G#2" : "file17.[mp3|ogg]",
+			"A2" : "file16.[mp3|ogg]",
+			"A#2" : "file15.[mp3|ogg]",
+			"B2" : "file14.[mp3|ogg]",
+			"C3" : "file13.[mp3|ogg]",
+	        "C#3" : "file12.[mp3|ogg]",
+	        "D3" : "file11.[mp3|ogg]",
+			"D#3" : "file10.[mp3|ogg]",
+			"E3" : "file9.[mp3|ogg]",
+			"F3" : "file8.[mp3|ogg]",
+			"F#3" : "file7.[mp3|ogg]",
+			"G3" : "file6.[mp3|ogg]",
+			"G#3" : "file5.[mp3|ogg]",
+			"A3" : "file4.[mp3|ogg]",
+			"A#3" : "file3.[mp3|ogg]",
+			"B3" : "file2.[mp3|ogg]",
+			"C4" : "file1.[mp3|ogg]"
+	
+				
+				
+					}, {
+
+						"baseUrl" : "/presets/jp4/Misc/PreciousLittleAngels/"
+					})					.connect(filter);
+					
